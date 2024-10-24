bd0156e5cb0f0ec783832c1fb7907534f2ed7d95
Вячеслав <bestlomo@yandex.ru>
2024-10-24 22:55:06 +0300

    Реализован метод read() получает тип класса в виде строки возвращает строку с полями если есть файл и пустую строку если нет

--- src/main/java/org/universalsort/service/ReadWriteService.java
+++ src/main/java/org/universalsort/service/ReadWriteService.java
@@ -1,7 +1,20 @@
 package org.universalsort.service;
 
-public class ReadWriteService {
-    public void read(){
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class ReadWriteService {
+    String classFields = ""; //поля класса в виде строки
+    public String read(String typeClass){
+
+        try {
+            Path path = Paths.get(typeClass + ".dat");
+            classFields = Files.readString(path);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return classFields;
     }
 }
