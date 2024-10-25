53b8f548f9554efddb46051f01913144a9158fe6
Sl <slaventyltd@gmail.com>
2024-10-25 12:25:00 +0300

    изменена методика получения полей класса

--- src/main/java/org/universalsort/service/ReadWriteService.java
+++ src/main/java/org/universalsort/service/ReadWriteService.java
@@ -1,20 +1,51 @@
 package org.universalsort.service;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * Для получения полей класса необходимо:
+ * 1 установить класс скоторым работаем
+ * 2 осуществить чтение или запись
+ * 3 получить поля класса в виде String методом getFields()
+ * */
 public class ReadWriteService {
-    String classFields = ""; //поля класса в виде строки
-    public String read(String typeClass){
+    String  path = "";  //путь до файла
+    String classFields; //поля класса
 
-        try {
-            Path path = Paths.get(typeClass + ".dat");
-            classFields = Files.readString(path);
-
-        }catch (Exception e){
-            e.printStackTrace();
+    //установка файла - класса с которым работаем и проверка на доступность файла
+    public boolean setClassCollection(String classCollection){
+        Path p = Paths.get(classCollection + ".dat");
+        if (Files.isReadable(p)){
+            path = classCollection + ".dat";
+            return true;
         }
+        return false;
+    }
+    //Получаем поля класса в виде строки
+    public String getClassFields(){
+        if (!classFields.isEmpty()){
+            return classFields;
+        }
+        System.out.println("Отсутствует файл или не установлен путь");
         return classFields;
+    }
+    //Чтение из файла
+    public void read(){
+        if (!path.isEmpty()){
+            try {
+                classFields = Files.readString(Path.of(path));
+            }catch (IOException e){
+                e.printStackTrace();
+            }catch (OutOfMemoryError out){
+                out.printStackTrace();
+            }catch (SecurityException se){
+                se.printStackTrace();
+            }
+        }else {
+            System.out.println("Отсутствует файл");
+        }
     }
 }
