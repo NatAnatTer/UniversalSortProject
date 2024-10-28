844982d2299bdb6d92b4ad16a5a3b471a90b76d8
Sl <slaventyltd@gmail.com>
2024-10-28 11:51:32 +0300

    изменил StrRndNmb.java - добавил статический метод Integer[] getMasIntRnd(int length) - добавил статический метод List<Integer> getListIntRnd(int length) при использовании требуют обработки исключения NegativeArraySizeException

--- src/main/java/org/universalsort/service/StrRndNmb.java
+++ src/main/java/org/universalsort/service/StrRndNmb.java
@@ -1,22 +1,23 @@
 package org.universalsort.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Формирует строковую последовательность случайных чисел
  * метод статический
  * */
 public class StrRndNmb {
-    private static final String[] defMass = {"1", "2", "3"};
-    public static String[] getStrRndNmb(int length){
-        try {
-            String[] str = new String[length];
-            for (int i = 0; i < length; i++) {
-                str[i] = String.valueOf((int) (Math.random() * 10));
-            }
-            return str;
-        }catch (Exception e){
-            e.printStackTrace();
+
+    public static Integer[] getMasIntRnd(int length)throws NegativeArraySizeException{
+        Integer[] integers = new Integer[length];
+        for (int i = 0; i < length; i++) {
+            integers[i] = (int) (Math.random() * 10);
         }
-        System.out.println("Размер массива меньше 0 возврат дефолтного массива");
-        return defMass;
+        return integers;
+    }
+    public static List<Integer> getListIntRnd(int length)throws NegativeArraySizeException{
+        return new ArrayList<>(List.of(getMasIntRnd(length)));
     }
 
 }
